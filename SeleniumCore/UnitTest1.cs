58fa5e168ca08c2262878217ff3119ed01aa6574
kareemsoom <kareemsoom@yahoo.com>
2019-03-14 14:07:58 +0100

    SeleniumChrome

--- SeleniumCore/UnitTest1.cs
+++ SeleniumCore/UnitTest1.cs
@@ -1,13 +1,24 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
 
 namespace SeleniumCore
 {
     [TestClass]
-    public class UnitTest1
+    public class HomepageFeature
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldBeAbleToLogin()
         {
+            string outputDirectory = Path.GetDirectoryName
+                (Assembly.GetExecutingAssembly().Location);
+
+
+            var driver = new ChromeDriver(outputDirectory);
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            //driver.Close();
         }
     }
 }
